abcb433031bf8b3d9312f7525a560984d88036e2
Subhasis Gorai <subhasis.gorai@gmail.com>
2021-08-31 10:01:29 +0530

    Rabin-Karp string matching algorithm

--- algo/pattern_matching.py
+++ algo/pattern_matching.py
@@ -1,12 +1,13 @@
 from timeit import repeat
 
+
 # The worst-case running time of the brute-force method is O(nm)
 def brute_force(text, pattern):
     if text and pattern:
         n, m = len(text), len(pattern)
         for i in range(n - m + 1):
             for k in range(m):
-                if not text[i + k] == pattern[k]:
+                if text[i + k] != pattern[k]:
                     break
                 if k == m - 1:
                     return i
@@ -36,6 +37,38 @@ def boyer_moore(text, pattern):
     return -1 
 
 
+from functools import reduce 
+
+
+def rabin_karp(text, pattern):
+
+    def confirm_equal(str1, hash_1, str2, hash_2):
+        return hash_1 == hash_2 and str1 == str2 
+        
+    len_t, len_p = len(text), len(pattern)
+    if len_p > len_t:
+        return -1
+    
+    base = 26
+    hash_t = reduce(
+                lambda rs, c: base * rs + ord(c),
+                    text[:len_p], 0)
+    hash_p = reduce(
+                lambda rs, c: base * rs + ord(c),
+                    pattern, 0)
+    power_max = base ** max(len_p - 1, 0)
+    
+    for i in range(len_p, len_t):
+        if confirm_equal(text[i - len_p:i], hash_t, pattern, hash_p):
+            return i - len_p
+        hash_t -= ord(text[i - len_p]) * power_max
+        hash_t = hash_t * base + ord(text[i])
+    
+    if confirm_equal(text[-len_p:], hash_t, pattern, hash_p):
+        return len_t - len_p
+    return -1
+
+
 if __name__ == '__main__':
     print brute_force('Hello World', 'lo Wo')
     print brute_force('Hello World', 'lo wo')
@@ -43,6 +76,10 @@ if __name__ == '__main__':
     print boyer_moore('Hello World', 'lo Wo')
     print boyer_moore('Hello World', 'lo wo')
     
+    print rabin_karp('Hello World', 'lo Wo')
+    print rabin_karp('Hello World', 'lo wo')
+    
+    
     print repeat('brute_force(\'Hello World\', \'lo Wo\')', 'from algo.pattern_matching import brute_force', repeat=3)
     print repeat('boyer_moore(\'Hello World\', \'lo Wo\')', 'from algo.pattern_matching import boyer_moore', repeat=3)
-                    
+    print repeat('rabin_karp(\'Hello World\', \'lo Wo\')', 'from algo.pattern_matching import rabin_karp', repeat=3)                
